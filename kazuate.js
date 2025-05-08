14a227e073f5a1ea6015bf1affa4f3c2621891b0
hana <248625@st.takushoku-u.ac.jp>
2025-05-08 16:57:39 +0900

    完成？

--- kazuate.js
+++ kazuate.js
@@ -17,8 +17,20 @@ hantei();
 function hantei() {
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   let yoso = 4;
-  
+  kaisu=kaisu+1;
+  console.log(kaisu+"回目の予想: "+yoso);
   // 課題3-1: 正解判定する
   // kotae と yoso が一致するかどうか調べて結果を出力
   // 課題3-1における出力先はコンソール
+  if(kaisu>=4){
+    console.log("答えは "+kotae+" でした．すでにゲームは終わっています");
+  }else if(kotae==yoso){
+    console.log("正解です．おめでとう!")
+  }else if(kaisu==3){
+    console.log("まちがい．残念でした答えは "+kotae+" です．");
+  }else if(kotae>yoso){
+    console.log("まちがい．答えはもっと大きいですよ");
+  }else{
+    console.log("まちがい．答えはもっと小さいですよ");
+  }
 }
