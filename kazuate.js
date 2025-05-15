07c6907b6f6cda29cebc3f4a805c40f4a2f05391
hana <248625@st.takushoku-u.ac.jp>
2025-05-15 16:38:13 +0900

    数当て完成

--- kazuate.js
+++ kazuate.js
@@ -5,21 +5,22 @@ console.log('答え（デバッグ用）: ' + kotae);
 // 入力回数（予想回数）
 let kaisu = 0;
 
-// 予想を4回実行する
-// 将来以下の hantei(); の4回の呼び出しを全て削除する
-// 代わりにここでは，ボタンを押したら hantei() を呼び出すイベント処理をする
+// ボタンを押したら hantei() を呼び出すイベント処理をする
+let b = document.querySelector('button#print');
+b.addEventListener('click', hantei); 
 
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-  // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
   kaisu=kaisu+1;
-  // 課題3-1: 正解判定する
-  // kotae と yoso が一致するかどうか調べて結果を出力
+// テキストボックスに指定された数値を yoso に代入する
   let i = document.querySelector('input[name="yoso"]');
   yoso = i.value;
   n = Number(yoso);
-  
-  // 課題3-1における出力先はコンソール
+  kaisuprint = document.querySelector('span#kaisu');
+  kaisuprint.textContent = kaisu;
+
+// 課題3-1: 正解判定する
+// kotae と yoso が一致するかどうか調べて結果を出力
   let p = document.querySelector('span#answer');
   if(kaisu>=4){
     p.textContent = ("答えは "+kotae+" でした．すでにゲームは終わっています");
@@ -33,6 +34,3 @@ function hantei() {
     p.textContent = ("まちがい．答えはもっと小さいですよ");
   }
 }
-
-let b = document.querySelector('button#print');
-b.addEventListener('click', hantei); 
