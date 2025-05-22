f7a9d1c2fefae2982942a48b94afe97ef1be6129
hana <248625@st.takushoku-u.ac.jp>
2025-05-22 15:02:43 +0900

    5-6

---
+++ practice-dom2/dom2-intro.js
@@ -0,0 +1,40 @@
+b = document.querySelector('button#henkou');
+b.addEventListener('click', changeDom);
+
+function changeDom() {
+let l = document.createElement('li'); 
+l.textContent = 'ヨット'; 
+let u = document.querySelector('ul#kazoeuta') 
+u.insertAdjacentElement('beforeend',l); 
+
+l = document.createElement('li'); 
+u.insertAdjacentElement('beforeend',l); 
+l.textContent = 'ごましお'; 
+
+let i = document.querySelector('img#bluemoon');
+i.setAttribute('src', 'bluemoon.jpg');
+
+let a = document.createElement('a');
+a.textContent = '拓殖大学HP';
+a.setAttribute('href', 'https://www.takushoku-u.ac.jp');
+let p = document.querySelector('p#takudai');
+p.insertAdjacentElement('afterend', a);
+
+l = document.querySelector('li#mochi');
+l.remove(); 
+l = document.querySelector('ul#kassen');
+l.remove(); 
+
+p = document.querySelector('p#primary');
+u = document.createElement('ul');
+p.insertAdjacentElement('afterend', u);
+l = document.createElement('li');
+u.insertAdjacentElement('beforeend', l);
+l.textContent = '赤';
+l = document.createElement('li');
+u.insertAdjacentElement('beforeend', l);
+l.textContent = '緑';
+l = document.createElement('li');
+u.insertAdjacentElement('beforeend', l);
+l.textContent = '青';
+}
